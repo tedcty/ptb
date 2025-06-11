99e79994ac8b7226baf551e62fcfe0ee9eb73eb5
tyeu008 <tyeu008@aucklanduni.ac.nz>
2025-06-12 08:32:21 +1200

    updated requirement setup .py file - removed specific version requirement

--- python_lib/ptb_src/requirements.py
+++ python_lib/ptb_src/requirements.py
@@ -3,7 +3,7 @@ import os
 if __name__ == '__main__':
     print("Getting Requirement:")
     os.system('conda install -c opensim-org opensim')
-    os.system('python -m pip install pandas scipy numpy==1.26.4 PySide6 tsfresh vtk')
+    os.system('python -m pip install pandas scipy numpy PySide6 tsfresh vtk')
     # for building the package
     os.system('python -m pip install build')
     print("Done!")
