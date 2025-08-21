c73f2f57897964eada07af982642198548588b9a
tedcty <tedyeung@outlook.com>
2025-08-22 11:35:23 +1200

    update ptb with pymeshlab helper for alpha wrap and remesh

--- python_lib/ptb_src/requirements.py
+++ python_lib/ptb_src/requirements.py
@@ -3,7 +3,7 @@ import os
 if __name__ == '__main__':
     print("Getting Requirement:")
     os.system('conda install -c opensim-org opensim')
-    os.system('python -m pip install pandas scipy numpy PySide6 tsfresh vtk')
+    os.system('python -m pip install pandas scipy numpy PySide6 tsfresh vtk pymeshlab')
     # for building the package
     os.system('python -m pip install build toml')
     print("Done!")
